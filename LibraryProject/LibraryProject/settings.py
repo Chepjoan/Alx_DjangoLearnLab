982b6411cbbb41cce8be59cfd8aa15c161a55e73
Joan Chep <chepjoan263@gmail.com>
2025-09-11 21:27:32 +0300

    Add CustomUser model, manager, admin setup, and settings update

--- LibraryProject/LibraryProject/settings.py
+++ LibraryProject/LibraryProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf',
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
@@ -121,4 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
