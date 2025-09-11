ac433821b2b80513d4a9504ff69b27b327f746f9
Joan Chep <chepjoan263@gmail.com>
2025-09-11 08:00:32 +0300

    Implement custom user model with CustomUserAdmin and update settings

--- Introduction_to_Django/LibraryProject/bookshelf/settings.py
+++ Introduction_to_Django/LibraryProject/bookshelf/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf',
     'bookshelf',   # 👈 added here
+    'relationship_app',
 ]
 
 MIDDLEWARE = [
@@ -122,4 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'bookshelf.CustomUser'
