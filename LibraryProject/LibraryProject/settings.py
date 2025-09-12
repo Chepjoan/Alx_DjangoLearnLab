5e4bc08a88140142f400fd0f79ecfd19e4c29f3a
Chepjoan <Chepjoan263@gmail.com>
2025-09-12 18:45:29 +0300

    Update settings.py

--- LibraryProject/LibraryProject/settings.py
+++ LibraryProject/LibraryProject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf',
-    'bookshelf',
+    'accounts',   # 👈 Add this line
+
 ]
 
 MIDDLEWARE = [
