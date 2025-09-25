2f8f79fe2aa6acad9daf1c80577b9bf6efada122
Joan Chep <chepjoan263@gmail.com>
2025-09-25 21:38:58 +0300

    Add DRF ListView, UpdateView, DeleteView with urls and permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,14 +2,26 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
-# List all books or create a new one
-class BookListCreateView(generics.ListCreateAPIView):
+# List all books
+class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-# Retrieve, update, or delete a single book
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+# Retrieve a single book
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+# Update an existing book
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+# Delete a book
+class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
